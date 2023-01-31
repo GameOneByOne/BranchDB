7fa50a70fe5281045b6d24c891a3176ae5197612
zhangyan <yanzhang.a2@gmail.com>
2023-01-31 22:30:54 +0800

    :bug: 修复错误的输入没有return的问题

--- main.cpp
+++ main.cpp
@@ -7,13 +7,13 @@ const int PARAMS_NUM = 2;
 
 int main(int argc, const char *argv[]) {
     if (argc != PARAMS_NUM) {
-        std::cout << "[Help] ./branchDB configFile." << std::endl;
+        std::cout << "[Help] ./branchDB configFile" << std::endl;
+        return 0;
     }
 
     if (!Config::Instance().Init(argv[1])) {
         return 0;
     }
-
 
     std::cout << "Hello!" << std::endl;
     return 0;
